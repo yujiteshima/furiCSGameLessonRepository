f0ae6b68467f5be4370359361981d538df3e8870
Yuji Teshima <yuji.teshima@gmail.com>
2021-01-03 19:56:40 +0900

    create wall
    create wall

---
+++ Assets/Bottom.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bottom : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(other.gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
